9ffe134edc086170f90459bdd61b1044bb7ddadd
SysMC <mbee@sysmc.ru>
2018-04-26 09:30:13 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+﻿from distutils.core import setup
+
+setup(
+    name='python-mbee',
+    version="1.0",
+    author='von Boduen',
+    author_email='mbee@sysmc.ru',
+    packages=['mbee'],
+    package_data = {'mbee': ['doc/*/*.*']},
+    url='https://github.com/SysMC/python-mbee',
+    license='MIT',
+    description='Python tools for working with MBee radios.',
+    long_description=open('README.txt').read(),
+    requires=['pySerial']
+)
